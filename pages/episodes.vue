34161358765ea058c27b59aaf015084ff4b9896d
Doucara Fodié <fodiedoucara8@gmail.com>
2022-08-25 18:05:42 +0200

    100%

--- pages/episodes.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Saison />
-</template>
-
-<script>
-export default {
-  name: 'EpisodePage'
-}
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Accueil />
+   <Saison />
 </template>
 
 <script>
