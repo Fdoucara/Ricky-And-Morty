927156b867b1dc8f285bbd4af2bf222b11c5f90a
Doucara Fodié <fodiedoucara8@gmail.com>
2022-09-09 00:04:21 +0200

    mep header + body

--- components/Episode.vue
+++ components/Episode.vue
@@ -4,23 +4,28 @@
       class="saison_episode_container_item_card"
       v-for="(episode, index) in saison"
       :key="index"
+      @click="toggleModale"
     >
       <div class="saison_episode_container_item_card_image">
-        <img :src="card_image[id]" alt="Rick et Morty"/>
+        <img :src="card_image[id]" alt="Rick et Morty" />
       </div>
 
       <div class="saison_episode_container_item_card_body">
         <p>{{ episode.episode }}</p>
         <p>{{ episode.air_date }}</p>
       </div>
+
+      <Modale :id="episode.id" :toggle="toggle"/>
     </div>
   </div>
 </template>
 
 <script>
+import Modale from "./Modale.vue";
 export default {
   name: "Episode",
   props: ["saison", "index"],
+  components: { Modale },
   data() {
     return {
       card_image: [
@@ -30,11 +35,17 @@ export default {
         "https://tse4.mm.bing.net/th?id=OIP.L32Q3mMbsVu931uN1KmTgAHaEK&pid=Api&P=0",
         "https://tse1.mm.bing.net/th?id=OIP.if2yG2TSxJfI5XlGxUgivQHaEK&pid=Api&P=0",
       ],
-      id: this.index
+      id: this.index,
+      toggle: false
     };
   },
-  mounted () {
-    this.$watch (
+  methods: {
+    toggleModale() {
+      this.toggle = !this.toggle;
+    }
+  },
+  mounted() {
+    this.$watch(
       () => this.index,
       () => {
         this.id = this.index;
---
+++ components/Modale.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modale" v-if="toggle">
+    <div class="overlay"></div>
+    <div class="modale_header"></div>
+    <div class="modale_body"></div>
+    <div class="modale_footer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id", "toggle"],
+  data() {
+    return {};
+  },
+  methods: {
+    recupData() {
+      this.$axios.$get("episode/" + this.id).then((reponse) => {
+        console.log(reponse);
+      });
+    },
+  }
+};
+</script>
