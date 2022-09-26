9904d92230cfeb168d66e33e391615b1091c78af
Doucara Fodié <fodiedoucara8@gmail.com>
2022-09-26 13:30:26 +0200

    mep body 55%

---
+++ components/Distribution.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="saison_episode_focus_characters_container">
+    <div
+      class="saison_episode_focus_characters_container_card"
+      v-for="(perso, index) in epPerso"
+      :id="perso.id"
+      :key="index"
+    >
+      <img
+        class="saison_episode_focus_characters_container_card_image"
+        :src="perso.image"
+        alt="Photo personnage"
+      />
+
+      <div class="saison_episode_focus_characters_container_card_contain" :id="perso.id">
+        <p>{{ perso.name }}</p>
+        <p>{{ perso.species }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Distribution',
+  props: ['epPerso'],
+  data() {
+    return {};
+  },
+};
+</script>
--- components/OneEpisode.vue
+++ components/OneEpisode.vue
@@ -1,67 +1,76 @@
 <template>
-  <div class="saison_episode_focus_container">
-    <div class="saison_episode_focus_container_card">
-      <img
-        class="saison_episode_focus_container_card_image"
-        :src="card_image"
-        alt="Rick et Morty"
-      />
+  <div>
+    <div class="saison_episode_focus_container">
+      <div class="saison_episode_focus_container_card">
+        <img
+          class="saison_episode_focus_container_card_image"
+          :src="card_image"
+          alt="Rick et Morty"
+        />
 
-      <div class="saison_episode_focus_container_card_contain">
-        <p>{{ epName }}</p>
-        <p>{{ epDate }}</p>
+        <div class="saison_episode_focus_container_card_contain">
+          <p>{{ epName }}</p>
+          <p>{{ epDate }}</p>
+        </div>
+      </div>
+
+      <div class="saison_episode_focus_container_button">
+        <div class="saison_episode_focus_container_button_item">
+          <p>Lecture épisode {{ epNum }}</p>
+        </div>
+        <div class="saison_episode_focus_container_button_item">
+          <p>Liste de favoris</p>
+        </div>
+        <div class="saison_episode_focus_container_button_item">
+          <p>Télécharger l'épisode</p>
+        </div>
       </div>
     </div>
 
-    <div class="saison_episode_focus_container_button">
-      <div class="saison_episode_focus_container_button_item">
-        <p> Lecture épisode {{ epNum }} </p>
-      </div>
-      <div class="saison_episode_focus_container_button_item">
-        <p> Liste de favoris </p>
-      </div>
-      <div class="saison_episode_focus_container_button_item">
-        <p> Télécharger l'épisode </p>
-      </div>
+    <div class="saison_episode_focus_characters">
+      <Distribution :epPerso="epPerso"/>
     </div>
   </div>
 </template>
 
 <script>
+import Distribution from "./Distribution.vue";
 export default {
-  props: ["id", "card_image"],
-  data() {
-    return {
-      info: null,
-      epName: "",
-      epNum: "",
-      epDate: "",
-      epPerso: [],
-    };
-  },
-  methods: {
-    getEpInfo() {
-      this.$axios.$get("episode/" + this.id).then((reponse) => {
-        this.info = reponse;
-        this.epName = reponse.name;
-        this.epNum = reponse.episode;
-        this.epDate = reponse.air_date;
-        for (let perso of reponse.characters) {
-          this.epPerso.push(perso);
-        }
-        console.log("Perso ", this.epPerso);
-      });
+    props: ["id", "card_image"],
+    data() {
+        return {
+            info: null,
+            epName: "",
+            epNum: "",
+            epDate: "",
+            epPerso: [],
+        };
     },
-  },
-  mounted() {
-    this.getEpInfo();
-    this.$watch(
-      () => this.id,
-      () => {
-        this.id = this.id;
+    methods: {
+        getEpInfo() {
+          this.epPerso = [],
+            this.$axios.$get("episode/" + this.id).then((reponse) => {
+                this.info = reponse;
+                this.epName = reponse.name;
+                this.epNum = reponse.episode;
+                this.epDate = reponse.air_date;
+                for (let perso of reponse.characters) {
+                    let idPerso = new URL(perso);
+                    this.$axios.$get(idPerso).then((reponse) => {
+                        this.epPerso.push(reponse);
+                    });
+                }
+                console.log(this.epPerso);
+            });
+        },
+    },
+    mounted() {
         this.getEpInfo();
-      }
-    );
-  },
+        this.$watch(() => this.id, () => {
+            this.id = this.id;
+            this.getEpInfo();
+        });
+    },
+    components: { Distribution }
 };
 </script>
