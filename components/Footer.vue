41f6130851712573f38470cea03d7d94ed000e6f
Doucara Fodié <fodiedoucara8@gmail.com>
2022-10-06 13:17:56 +0200

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="footer">
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, esse.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, esse.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, esse.</p>
+    <div class="footer_image">
+      <img src="../assets/images/rick-and-morty-31040.png" class="footer_image_img1">
+      <img src="../assets/images/rickMorty.png" class="footer_image_img2">
+    </div>
+    <div class="footer_contain">
+      <p> Fodié DOUCARA - 2022 <fa-icons class="fab fa-github"/> </p>
+      <i class="fab fa-github"></i>
+    </div>
   </div>
 </template>
 
